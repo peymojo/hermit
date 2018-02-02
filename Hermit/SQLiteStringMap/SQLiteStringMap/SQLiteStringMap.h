b935e2b9d3b8b58ff12f5ce5d15bb81451b8cddb
Paul Young <peymojo@gmail.com>
2018-02-02 13:03:04 +0700

    Add static lib target for SQLiteStringMap.

---
+++ Hermit/SQLiteStringMap/SQLiteStringMap/SQLiteStringMap.h
@@ -0,0 +1,13 @@
+//
+//  SQLiteStringMap.h
+//  SQLiteStringMap
+//
+//  Created by Paul Young on 2/2/18.
+//  Copyright © 2018 filehaven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQLiteStringMap : NSObject
+
+@end
---
+++ Hermit/SQLiteStringMap/SQLiteStringMap/SQLiteStringMap.m
@@ -0,0 +1,13 @@
+//
+//  SQLiteStringMap.m
+//  SQLiteStringMap
+//
+//  Created by Paul Young on 2/2/18.
+//  Copyright © 2018 filehaven. All rights reserved.
+//
+
+#import "SQLiteStringMap.h"
+
+@implementation SQLiteStringMap
+
+@end
